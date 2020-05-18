fe73839de9ee35b17a1277ae7f041925458e3457
HirokiSatoh <sh.0921.soccer@outlook.jp>
2020-05-18 17:02:33 +0900

    favorites

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   root to:'home#top'
   get 'home/about', to:'home#about'
   devise_for :users
-  resources :books,only: [:new, :create, :index, :show, :edit, :destroy, :update]
-  resources :users
-end 
+  resources :users,only: [:new, :create, :index, :show, :edit, :destroy, :update]
+  resources :books do
+  	resources :favorites, only:[:create, :destroy]
+  end
+end
